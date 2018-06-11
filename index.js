ed256b62e7af58ab0b9843c82c628a1a0b124b62
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-11 08:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function add(a,b) {
-  (a) + (b);
+function add(a, b) {
+  a + b
 }
 
-function subtract(a,b) {
-  (a - b);
+function subtract(a, b) {
+  a - b;
 }
 
-function multiply(a,b) {
-  a*b;
+function multiply(a, b) {
+  a * b;
 }
 
-function divide(a,b) {
-  a/b;
+function divide(a, b) {
+  a / b;
 }
